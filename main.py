447c22c38d3456469e169d04df725cbdc9cdf5dc
Jason Miller <Jason@example.com>
2025-10-23 12:17:16 -0700

    messages pre-loop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 load_dotenv()
@@ -11,10 +12,14 @@ client = genai.Client(api_key=api_key)
 def main():
     if len(sys.argv) < 2:
         raise  IndexError("No prompt provided")
-        sys.exit(1)
 
-    prompt = sys.argv[1]
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=prompt)
+    
+
+    prompt = sys.argv[1:]
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+]
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
